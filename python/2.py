6e769bdc548d485ba2c46b82af4642aad3d63d89
AndreaCaval <andrea.cavalluzzo2000@gmail.com>
2021-02-02 11:15:51 +0100

    02/02/21

---
+++ python/2.py
@@ -0,0 +1,23 @@
+'''
+INPUT: sequenza di numeri terminata da 0
+OUTPUT: massimo e minimo inseriti
+'''
+
+n = 1
+lista = []
+
+while n != 0:
+
+    try:
+        n = int(input("Inserisci un numero: "))
+        if(n != 0):
+            lista.append(n)
+    except:
+        print("Errore di inserimento")
+
+if(len(lista) != 0):
+    print("Lista= " + str(lista))
+    print("Minimo: " + str(min(lista)))
+    print("Massimo: " + str(max(lista)))
+else:
+    print("La lista è vuota")
